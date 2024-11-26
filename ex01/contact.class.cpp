dcba49fe7ca706cbf6cc0ef62c66222c8b4a68b7
SimArch21 <simon.archambault@hotmail.fr>
2024-11-26 20:38:37 +0100

    commit

--- ex01/contact.class.cpp
+++ ex01/contact.class.cpp
@@ -6,7 +6,16 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:14:47 by simarcha          #+#    #+#             */
-/*   Updated: 2024/11/25 13:14:48 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/11/26 20:38:14 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "contact.class.hpp"
+
+Contact::Contact(int idx, const std::string& fn, const std::string& ln, 
+	const std::string& nick, const std::string& phone, const std::string& secret)
+	: index(idx),  first_name(fn), last_name(ln), nickname (nick), phone_number(phone),
+	darkest_secret(secret)
+{}
+
+Contact::~Contact(void) {}
--- ex01/contact.class.hpp
+++ ex01/contact.class.hpp
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:15:15 by simarcha          #+#    #+#             */
-/*   Updated: 2024/11/26 18:35:29 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/11/26 19:21:36 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,24 @@
 class	Contact
 {
 	private:
-		int			index;
-		std::string	first_name;
-		std::string	last_name;
-		std::string	nickname;
-		std::string	phone_number;
-		std::string	darkest_secret;
+		int					index;
+		const std::string	first_name;
+		const std::string	last_name;
+		const std::string	nickname;
+		const std::string	phone_number;
+		const std::string	darkest_secret;
 
 	public:
 		Contact(int idx, const std::string& fn, const std::string& ln, const std::string& nick,
-		const std::string& phone, const std::string& secret)
-		: index(idx),  first_name(fn), last_name(ln), nickname (nick), phone_number(phone),
-		darkest_secret(secret) {}
-		// ~Contact(void);
+		const std::string& phone, const std::string& secret);
+		~Contact(void);
 
+	int			get_contact_index() const {return index;}
 	std::string get_first_name() const {return first_name;}
 	std::string get_last_name() const {return last_name;}
 	std::string get_nickname() const {return nickname;}
 	std::string get_phone_number() const {return phone_number;}
 	std::string get_darkest_secret() const {return darkest_secret;}
-	int			get_contact_index() {return index;}
 };
 
 #endif
