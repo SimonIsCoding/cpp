ed238dbaed5e66b43b071d35afa3fc113c07e34b
SimArch21 <simon.archambault@hotmail.fr>
2024-11-29 10:47:04 +0100

    search first step done

---
+++ ex01_1/contact.class.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.class.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 13:14:47 by simarcha          #+#    #+#             */
+/*   Updated: 2024/11/28 19:01:18 by simarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "contact.class.hpp"
+#include <iostream>
+
+Contact::Contact(void){}
+
+Contact::~Contact(void) {}
+
+void	Contact::set_first_name(std::string str)
+{
+	this->_first_name = str;
+}
+
+void	Contact::set_last_name(std::string str)
+{
+	this->_last_name = str;
+}
+
+void	Contact::set_nickname(std::string str)
+{
+	this->_nickname = str;
+}
+
+void	Contact::set_phone_number(std::string str)
+{
+	this->_phone_number = str;
+}
+
+void	Contact::set_darkest_secret(std::string str)
+{
+	this->_darkest_secret = str;
+}
+
+void	Contact::set_index(int idx)
+{
+	this->index = idx;
+}
+
+/*std::string Contact::get_first_name(void) const
+{
+	return (this->_first_name);
+}
+
+std::string	Contact::get_last_name(void) const
+{
+	return (this->_last_name);
+}
+
+std::string Contact::get_nickname(void) const
+{
+	return (this->_nickname);
+}
+
+std::string Contact::get_phone_number(void) const
+{
+	return (this->_phone_number);
+}
+
+std::string Contact::get_darkest_secret(void) const
+{
+	return (this->_darkest_secret);
+}*/
---
+++ ex01_1/main.cpp
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 14:49:48 by simarcha          #+#    #+#             */
+/*   Updated: 2024/11/29 10:44:22 by simarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "contact.class.hpp"
+#include "phonebook.class.hpp"
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+void	add_contact(Contact &contact, int *nb_contact)
+{
+	printf("ADD written\n");
+
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+
+	contact.set_index(++(*nb_contact));
+	std::cout << "Contact's first name: ";
+	std::cin >> first_name;
+	contact.set_first_name(first_name);
+	std::cout << "Contact's last name: ";
+	std::cin >> last_name;
+	contact.set_last_name(last_name);
+	std::cout << "Contact's nickname: ";
+	std::cin >> nickname;
+	contact.set_nickname(nickname);
+	std::cout << "Contact's phone_number: ";
+	std::cin >> phone_number;
+	contact.set_phone_number(phone_number);
+	std::cout << "Contact's darkest_secret: ";
+	std::cin >> darkest_secret;
+	contact.set_darkest_secret(darkest_secret);
+	std::cout << "index: " << contact.get_contact_index() << std::endl;
+	std::cout << "first_name: " << contact.get_first_name() << std::endl;
+	std::cout << "last_name: " << contact.get_last_name() << std::endl;
+	std::cout << "nickname: " << contact.get_nickname() << std::endl;
+	std::cout << "phone_number: " << contact.get_phone_number() << std::endl;
+	std::cout << "darkest_secret: " << contact.get_darkest_secret() << std::endl;
+
+	// printf("index = %i\n", *(nb_contact));
+	return ;
+}
+
+void	complete_field(std::string str)
+{
+	int	len;
+	int	whitespaces;
+	int	i;
+
+	i = -1;
+	// if (!str)
+	// 	return ;
+	len = str.size();
+	if (len > 10)
+	{
+		while (++i < 9)
+			std::cout << str[i];
+		std::cout << '.';
+	}
+	else
+	{
+		whitespaces = 10 - len;
+		while (whitespaces-- > 0)
+			std::cout << ' ';
+		while (++i < len)
+			std::cout << str[i];
+	}
+	std::cout << '|';
+}
+
+// void	fit_in_the_cell(char *str)
+// {
+// 	if (strlen(str) )
+// }
+
+void	search_contact(Contact& contact, int *nb_contact)
+{
+	printf("SEARCH written\n");
+	std::cout << " ___________________________________________" << std::endl;
+	std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+	std::cout << "|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|" << std::endl;
+	if (*nb_contact > 0)
+	{
+		std::cout << "|         " << contact.get_contact_index() << "|";
+		complete_field(contact.get_first_name());
+		complete_field(contact.get_last_name());
+		complete_field(contact.get_nickname());
+		// complete_field(contact.get_phone_number());
+		// complete_field(contact.get_darkest_secret());
+		std::cout << std::endl;
+		std::cout << " ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ " << std::endl;
+	}
+	printf("*nb_contact = %i\n", *nb_contact);
+	return ;
+}
+
+void	init_contact(Contact)
+{
+	
+}
+
+int	main(void)
+{
+	char		input[32];
+	int			nb_contact;
+	Contact		contact;
+
+	nb_contact = 0;
+	std::cout << "Welcome to the PhoneBook!\n";
+	std::cout << "Enter a command like ADD, SEARCH or EXIT: ";
+	std::cin >> input;
+	while (1)
+	{
+		while ((strcmp(input, "SEARCH") &&  strcmp(input, "ADD")
+				&& strcmp(input, "EXIT")))
+		{
+			std::cout << "Enter a command like ADD, SEARCH or EXIT: ";
+			std::cin >> input;
+		}
+		if (strcmp(input, "EXIT") == 0)
+			return (0);
+		else if (strcmp(input, "ADD") == 0)
+			add_contact(contact, &nb_contact);
+		else
+			search_contact(contact, &nb_contact);
+		std::cout << "Enter a command like ADD, SEARCH or EXIT: ";
+		std::cin >> input;
+	}
+	return (0);
+}
