7ad21cf3ef0304423e4f80e2e75dba4907737ade
SimArch21 <simon.archambault@hotmail.fr>
2024-12-09 14:17:27 +0100

    cleaning

--- ex01_1/contact.class.hpp
+++ ex01_1/contact.class.hpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:15:15 by simarcha          #+#    #+#             */
-/*   Updated: 2024/12/09 13:15:24 by simon            ###   ########.fr       */
+/*   Updated: 2024/12/09 14:15:48 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,12 @@ class	Contact
 	std::string get_phone_number() const {return _phone_number;}
 	std::string get_darkest_secret() const {return _darkest_secret;}
 
-	// void		set_index(int idx);
 	void		set_first_name(std::string str);
 	void		set_last_name(std::string str);
 	void		set_nickname(std::string str);
 	void		set_phone_number(std::string str);
 	void		set_darkest_secret(std::string str);
-	void		set_contact_details(Contact& contact, int nb_contact);
+	void		set_contact_details(Contact& contact);
 };
 
 #endif
