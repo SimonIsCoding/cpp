d5d3d7cf6169f05b85b94798b0916d84bed9b8d5
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2024-12-19 16:41:52 +0100

    ex06cpp01 done

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 14:07:39 by simon             #+#    #+#             */
-/*   Updated: 2024/12/19 14:25:48 by simon            ###   ########.fr       */
+/*   Updated: 2024/12/19 15:48:20 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 
 int	main(int argc, char **argv)
 {
+	Harl	harl;
+
 	if (argc != 2)
 		return (std::cerr << "Arguments not well written" << std::endl, 1);
 	if (strcmp(argv[1], "DEBUG") && strcmp(argv[1], "INFO")
 		&& strcmp(argv[1], "WARNING") && strcmp(argv[1], "ERROR"))
 		return (std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl, 0);
-	//send it the filter function
+	std::string level(argv[1]);
+	harl.complain(level);
 	return (0);
 }
