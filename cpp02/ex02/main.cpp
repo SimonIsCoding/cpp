a4451acc2a265969f0ed58c43f3ba22996524318
SimArch21 <simon.archambault@hotmail.fr>
2025-01-02 19:50:04 +0100

    starting cpp03

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:26:56 by simon             #+#    #+#             */
-/*   Updated: 2024/12/25 16:26:21 by simon            ###   ########.fr       */
+/*   Updated: 2024/12/25 16:41:41 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,25 @@ int	main(void)
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
 	
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+
+	std::cout << "test" << std::endl;
+	std::cout << b / a << std::endl;
+
+		Fixed c(1);
+	Fixed d(2);
+	std::cout << "c = " << c << std::endl;
+	std::cout << "d = " << d << std::endl;
+	std::cout << b * a << std::endl;
 
 	return (0);
 }
