ab8f5a3c16a845ba66f3762723496fdc669d0e32
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-01-07 20:27:35 +0100

    exo00 doing

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 16:44:41 by simon             #+#    #+#             */
-/*   Updated: 2025/01/05 16:44:57 by simon            ###   ########.fr       */
+/*   Updated: 2025/01/07 19:49:17 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int	main(void)
 {
-	
+	ClapTrap	Pikachu("Pikachu");
+	ClapTrap	Bulbizarre("Bulbizarre");
+
+	Pikachu.attack("Bulbizarre");
+		
+	return (0);
 }
