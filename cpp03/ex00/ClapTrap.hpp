c34dc92d309f85306508dfa6f8fa657650c57596
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-01-08 11:56:50 +0100

    exo 00 done

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 16:44:19 by simon             #+#    #+#             */
-/*   Updated: 2025/01/07 18:59:51 by simon            ###   ########.fr       */
+/*   Updated: 2025/01/08 11:05:14 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ class ClapTrap
 		void attack(const std::string& target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
+
+		void	print_status();
 };
 
 #endif
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 16:44:41 by simon             #+#    #+#             */
-/*   Updated: 2025/01/07 19:49:17 by simon            ###   ########.fr       */
+/*   Updated: 2025/01/08 11:55:43 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,32 @@ int	main(void)
 	ClapTrap	Bulbizarre("Bulbizarre");
 
 	Pikachu.attack("Bulbizarre");
-		
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	
+	Bulbizarre.beRepaired(3);
+	
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	Bulbizarre.takeDamage(1);
+	Pikachu.attack("Bulbizarre");
+	
+	std::cout << std::endl;
+	Pikachu.print_status();
+	Bulbizarre.print_status();
 	return (0);
 }
